bc215ad1f2a2494d115015a7b1a8991e32fbbf75
Benjamin Carlson <b.carlson@cablelabs.com>
2025-02-27 10:36:01 -0700

    Implemented baseline handling of Proxy DPP Encap Msg
    Signed-off-by: Benjamin Carlson <b.carlson@cablelabs.com>

--- src/em/prov/easyconnect/ec_util.cpp
+++ src/em/prov/easyconnect/ec_util.cpp
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <arpa/inet.h>
 
 #include "ec_util.h"
 #include "util.h"
@@ -74,6 +75,133 @@ uint16_t ec_util::freq_to_channel_attr(unsigned int freq)
     return ((channel << 8) | (0x00ff & op_class));
 }
 
+int ec_util::parse_dpp_chirp_tlv(em_dpp_chirp_value_t* chirp_tlv, uint16_t chirp_tlv_len, mac_addr_t *mac, uint8_t **hash, uint8_t *hash_len)
+{
+    if (chirp_tlv == NULL || chirp_tlv_len == 0) {
+        fprintf(stderr, "Invalid input\n");
+        return -1;
+    }
+
+    uint16_t data_len = chirp_tlv_len - sizeof(em_dpp_chirp_value_t);
+    // Parse TLV
+    bool mac_addr_present = chirp_tlv->mac_present;
+    bool hash_valid = chirp_tlv->hash_valid;
+
+    uint8_t *data_ptr = chirp_tlv->data;
+    if (mac_addr_present && data_len >= sizeof(mac_addr_t)) {
+        memcpy(*mac, data_ptr, sizeof(mac_addr_t));
+        data_ptr += sizeof(mac_addr_t);
+        data_len -= sizeof(mac_addr_t);
+    }
+
+    if (!hash_valid || data_len <= 0) {
+        // Clear (Re)configuration state, agent side
+        return 0;
+    }
+
+    *hash_len = *data_ptr;
+    data_ptr++;
+    if (data_len < *hash_len) {
+        fprintf(stderr, "Invalid chirp tlv\n");
+        return NULL;
+    }
+    memcpy(*hash, data_ptr, *hash_len);
+
+    return 0;
+}
+
+int ec_util::parse_encap_dpp_tlv(em_encap_dpp_t *encap_tlv, uint16_t encap_tlv_len, mac_addr_t *dest_mac, uint8_t *frame_type, uint8_t **encap_frame, uint8_t *encap_frame_len)
+{
+    if (encap_tlv == NULL || encap_tlv_len == 0) {
+        fprintf(stderr, "Invalid input\n");
+        return -1;
+    }
+
+    uint16_t data_len = encap_tlv_len - sizeof(em_encap_dpp_t);
+    // Parse TLV
+    bool mac_addr_present = encap_tlv->enrollee_mac_addr_present;
+
+    // Copy mac address if present
+    uint8_t *data_ptr = encap_tlv->data;
+    if (mac_addr_present && data_len >= sizeof(mac_addr_t)) {
+        memcpy(*dest_mac, data_ptr, sizeof(mac_addr_t));
+        data_ptr += sizeof(mac_addr_t);
+        data_len -= sizeof(mac_addr_t);
+    } else {
+        memset(*dest_mac, 0, sizeof(mac_addr_t));
+    }
+
+    if (data_len < sizeof(uint8_t) + sizeof(uint16_t)) {
+        fprintf(stderr, "Invalid encap tlv\n");
+        return -1;
+    }
+
+    // Get frame type
+    *frame_type = *data_ptr;
+    data_ptr++;
+
+    // Get frame length
+    *encap_frame_len = htons(*((uint16_t *)data_ptr));
+    data_ptr += sizeof(uint16_t);
+
+    if (data_len < *encap_frame_len) {
+        fprintf(stderr, "Invalid encap tlv\n");
+        return -1;
+    }
+
+    // Copy frame
+    memcpy(*encap_frame, data_ptr, *encap_frame_len);
+
+    return 0;
+}
+
+em_encap_dpp_t * ec_util::create_encap_dpp_tlv(bool dpp_frame_indicator, uint8_t content_type, mac_addr_t *dest_mac, uint8_t frame_type, uint8_t *encap_frame, uint8_t encap_frame_len)
+{
+    size_t data_size = sizeof(em_encap_dpp_t) + sizeof(uint8_t) + sizeof(uint16_t) + encap_frame_len;
+    if (dest_mac != NULL) {
+        data_size += sizeof(mac_addr_t);
+    }
+    em_encap_dpp_t *encap_tlv = NULL;
+
+    if ((encap_tlv = (em_encap_dpp_t *)calloc(data_size, 1)) == NULL){
+        fprintf(stderr, "Failed to allocate memory\n");
+        return NULL;
+    }
+    (encap_tlv)->dpp_frame_indicator = dpp_frame_indicator;
+    (encap_tlv)->content_type = content_type;
+    (encap_tlv)->enrollee_mac_addr_present = (dest_mac != NULL) ? 1 : 0;
+
+    uint8_t *data_ptr = (encap_tlv)->data;
+    if (dest_mac != NULL) {
+        memcpy(data_ptr, dest_mac, sizeof(mac_addr_t));
+        data_ptr += sizeof(mac_addr_t);
+    }
+
+    *data_ptr = frame_type;
+    data_ptr++;
+
+    *((uint16_t *)data_ptr) = htons(encap_frame_len);
+    data_ptr += sizeof(uint16_t);
+
+    memcpy(data_ptr, encap_frame, encap_frame_len);
+
+    return encap_tlv;
+}
+
+ec_frame_t *ec_util::copy_attrs_to_frame(ec_frame_t *frame, uint8_t *attrs, uint16_t attrs_len)
+{
+    uint16_t new_len = EC_FRAME_BASE_SIZE + attrs_len;
+    ec_frame_t* new_frame = (ec_frame_t *) realloc((uint8_t*)frame, new_len);
+    if (new_frame == NULL) {
+        printf("%s:%d unable to realloc memory\n", __func__, __LINE__);
+        return NULL; 
+    }
+    memcpy(new_frame->attributes, attrs, attrs_len);
+
+    return new_frame;
+
+}
+
 bool ec_util::validate_frame(const ec_frame_t *frame)
 {
     if ((frame->category != 0x04) 
@@ -87,6 +215,15 @@ bool ec_util::validate_frame(const ec_frame_t *frame)
     }
 
     return true;
+}
+
+std::string ec_util::hash_to_hex_string(const uint8_t *hash, size_t hash_len) {
+    char output[hash_len * 2 + 1];
+    for (size_t i = 0; i < hash_len; i++) {
+        sprintf(output + (i * 2), "%02x", hash[i]);
+    }
+    output[hash_len * 2] = '\0'; // Null-terminate the string
+    return std::string(output);
 }
 
 void ec_util::print_bignum (BIGNUM *bn)
